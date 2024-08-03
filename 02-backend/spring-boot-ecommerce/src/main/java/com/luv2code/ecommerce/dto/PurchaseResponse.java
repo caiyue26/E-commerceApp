3c02060349ae4dfab9c926e04ac029675191cf23
yuecai <yuecai466@gmail.com>
2024-08-03 13:47:35 -0400

    YueCai 23 & 24

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dto/PurchaseResponse.java
@@ -0,0 +1,9 @@
+package com.luv2code.ecommerce.dto;
+
+import lombok.Data;
+
+// use this class to send back a Java object as JSON
+@Data
+public class PurchaseResponse {
+    private final String orderTrackingNumber;
+}
